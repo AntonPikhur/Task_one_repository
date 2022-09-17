d2b56d3acaf1c647c4bebcad645ed1461977fa61
AntonPikhur <113526199+AntonPikhur@users.noreply.github.com>
2022-09-17 16:00:23 +0300

    Update ts1auto.spec.js

--- tests/ts1auto.spec.js
+++ tests/ts1auto.spec.js
@@ -1,51 +1,12 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
+const generatorPage = require ('../../helper/generator.page')
 
 test('test', async ({ page }) => {
 
-    function makeLogin(length) {
-        var result           = '';
-        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        var charactersLength = characters.length;
-        for ( var i = 0; i < length; i++ ) {
-          result += characters.charAt(Math.floor(Math.random() * 
-          charactersLength));
-        }
-        return result;
-      }
-
-    function makeEmail() {
-        var strValues = "abcdefg12345";
-        var strEmail = "";
-        var strTmp;
-        for (var i = 0; i < 10; i++) {
-            strTmp = strValues.charAt(Math.round(strValues.length * Math.random()));
-            strEmail = strEmail + strTmp;
-        }
-        strTmp = "";
-        strEmail = strEmail + "@";
-        for (var j = 0; j < 8; j++) {
-            strTmp = strValues.charAt(Math.round(strValues.length * Math.random()));
-            strEmail = strEmail + strTmp;
-        }
-        strEmail = strEmail + ".com"
-        return strEmail;
-    }
-
-    function makePass(length) {
-        var result           = '';
-        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        var charactersLength = characters.length;
-        for ( var i = 0; i < length; i++ ) {
-            result += characters.charAt(Math.floor(Math.random() * 
-        charactersLength));
-        }
-        return result;
-    }
-
-    const genLogin = makeLogin(6);
-    const genPass = makePass(8);
-    const genEmail = makeEmail();
+    const genLogin = generatorPage.makeLogin(6);
+    const genPass = generatorPage.makePass(8);
+    const genEmail = generatorPage.makeEmail();
 
   // Go to https://www.redmine.org/
   await page.goto('https://www.redmine.org/');
